8e16c9fce3a8f8d888754daad9f4c53424907c5e
Adarsh <adarsh800251@gmail.com>
2023-02-18 23:38:35 +0530

    laster

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -22,7 +22,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
     const main = palette.neutral.main;
     const medium = palette.neutral.medium;
   
-    const isFriend = false
+    // const isFriend = friends.find((friend) => friend._id === friendId);
   
     const patchFriend = async () => {
       const response = await fetch(
@@ -71,11 +71,11 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
           onClick={() => patchFriend()}
           sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
         >
-          {isFriend ? (
+          {/* {isFriend ? (
             <PersonRemoveOutlined sx={{ color: primaryDark }} />
           ) : (
             <PersonAddOutlined sx={{ color: primaryDark }} />
-          )}
+          )} */}
         </IconButton>
       </FlexBetween>
     );
